4957208e31341f62e7910e5286f9d0b23a96e9f5
Josh Rey <jrey1212@gmail.com>
2023-01-30 14:38:19 -0500

    First commit

---
+++ actions.js
@@ -0,0 +1,5 @@
+formatting.mount('#actions')
+
+// formatting.getElementById("welcomeBtn").style.backgroundColor = "yellow"; 
+welcomeBtn.onmouseover = function().style.backgroundColor="yellow";
+// object.addEventListener("mouseover", style.backgroundColor="yellow"); /*none of these are working :'( */
